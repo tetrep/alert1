61d8932177e50aaa0ec4281a193f093eb6b16f01
Paul Tetreau <paul@matasano.com>
2015-01-10 11:46:27 -0800

    body doesn't exist when we're loaded now, we need to inject into head

--- content.js
+++ content.js
@@ -150,7 +150,7 @@ this.inject_js = function () {
     this.save_settings(undefined, 'content');
   }
 
-  document.body.appendChild(document.createElement('script')).innerHTML = this.content.obj_js + nonce_js + hooked_functions_js + this.content.hook_js + this.content.hook_init_js + this.content.obj_name+'.init.bind('+this.content.obj_name+')();\n';
+  document.head.appendChild(document.createElement('script')).innerHTML = this.content.obj_js + nonce_js + hooked_functions_js + this.content.hook_js + this.content.hook_init_js + this.content.obj_name+'.init.bind('+this.content.obj_name+')();\n';
 };
 
 //listen for stack traces
